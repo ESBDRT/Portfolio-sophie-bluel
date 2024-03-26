b33d4c3fe1cd3cea8a99f441bbbc593698ab4242
Esteban DROUET <Esteban.Drouet94400@gmail.com>
2024-03-26 23:48:28 +0100

    Added first popup and delete option

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,50 +1,22 @@
-// Fonction filtrage catégories
-function filtering(categoryId) {
-    const figures = document.querySelectorAll('#gallery figure');
-    figures.forEach(figure => {
-        if (categoryId === '0' || parseInt( figure.getAttribute('data-category-id')) == parseInt(categoryId)) {
-            figure.style.display = 'block';
-        } else {
-            figure.style.display = 'none';
-        }
-    });
+// Requéte API Catégories
+async function FetchCategories() {
+    return await fetch('http://localhost:5678/api/categories').then(response => {
+        return response.json()
+    })
+  }
+
+// Requéte API Works  
+async function FetchWorks() {
+    return await fetch('http://localhost:5678/api/works').then(response => {
+        return response.json()
+    })
 }
 
-// Request API categories
-fetch('http://localhost:5678/api/categories').then(response => {
-    response.json().then(data => {
-        let html = '<button class="btn" data-id="0">Tous</button>'; // Adding "Tous" button
-        data.forEach(elt => {
-            html += '<button class="btn" data-id="' + elt.id + '">' + elt.name + '</button>';
-        });
-        document.getElementById('filters').innerHTML = html;
-
-        const buttons = document.querySelectorAll('#filters .btn');
-        buttons.forEach(button => {
-            button.addEventListener('click', function() {
-                const categoryId = this.getAttribute('data-id');
-                filtering(categoryId);
-            });      
-        });       
-    });    
-});
-
-// Request API works 
-fetch('http://localhost:5678/api/works').then(response => {
-    response.json().then(data => {
-        let html = "";
-        data.forEach(elt => {
-            html += '<figure data-category-id="' + elt.category.id + '"><img src="'+elt.imageUrl+'" alt="'+elt.title+'"> <figcaption>'+elt.title+'</figcaption> </figure>';
-        });
-        document.getElementById('gallery').innerHTML = html;
-    });
-});
-
-// Fonction login
+// Requéte API login
 function sendDataToAPI(event) {
     event.preventDefault(); // Prevent form submission
 
-    // Retrieve input values
+    
     var email = document.getElementById("emailInput").value;
     var password = document.getElementById("passwordInput").value;
 
@@ -71,16 +43,61 @@ function sendDataToAPI(event) {
         console.log(data);
         var token = data.token;
         localStorage.setItem('token', token);
-        isUserLoggedIn()
     })
 }
 
-// Fonction pour clear le local Storage
+// Filtrage 
+function filtering(categoryId) {
+    const figures = document.querySelectorAll('#gallery figure');
+    figures.forEach(figure => {
+        if (categoryId === '0' || parseInt( figure.getAttribute('data-category-id')) == parseInt(categoryId)) {
+            figure.style.display = 'block';
+        } else {
+            figure.style.display = 'none';
+        }
+    });
+}
+
+// Créations boutons
+async function renderFilters() {
+    const filters = await FetchCategories();
+    let html = '<button class="btn" data-id="0">Tous</button>';
+    filters.forEach(elt => {
+      html += '<button class="btn" data-id="' + elt.id + '">' + elt.name + '</button>';
+    });
+    document.getElementById('filters').innerHTML = html;
+  
+    const buttons = document.querySelectorAll('#filters .btn');
+    buttons.forEach(button => {
+      button.addEventListener('click', function() {
+        const categoryId = this.getAttribute('data-id');
+        filtering(categoryId);
+      });
+    });
+}
+
+renderFilters()
+
+// Création galerie
+async function renderGallery() {
+    const works = await FetchWorks()
+        let html = "";
+            works.forEach(elt => {
+            html += '<figure data-category-id="' + elt.category.id + '" data-id="' + elt.id + '"><img src="'+elt.imageUrl+'" alt="'+elt.title+'"> <figcaption>'+elt.title+'</figcaption> </figure>';
+        });
+            document.getElementById('gallery').innerHTML = html;
+ }
+
+renderGallery()
+
+// Clear local storage
 function clearStorage() {
     localStorage.removeItem('token'); 
     isUserLoggedIn()
     window.location.href ="login.html"
 };
+
+document.getElementById('logout-btn').addEventListener("click", clearStorage)
 
 // Fonction pour modifier l'affichage quand l'utilisateur est login
 function loggedInDisplay() {
@@ -89,7 +106,6 @@ function loggedInDisplay() {
     const editBanner = document.getElementById("edit-banner");
     const divEdit = document.getElementById("div-edit");
     const overlay = document.getElementById("overlay");
-    const popup = document.getElementById("popup");
 
     if (token !== null) {
         filters.style.display = "none";
@@ -115,34 +131,68 @@ function isUserLoggedIn() {
     }
 }
 
-// Request api works pour la popup
-function FetchPopup() {
-    fetch('http://localhost:5678/api/works').then(response => {
-    response.json().then(data => {
-        console.log(data)
-        let html = "";
-        data.forEach(elt => {
-            html += '<a><img src="' + elt.imageUrl + '" class="popup-images"><i class="fa-solid fa-trash-can delete-logo"></i></a>'
-        });
-        document.getElementById('popup-gallery').innerHTML = html;
-    });
-});}
+isUserLoggedIn()
 
-// Fonction pour ouvrir la popup
-function openPopup() {
+
+// Ouvrir popup
+async function openPopup() {
     const overlay = document.getElementById('overlay');
     const popup = document.getElementById('popup')
 
     overlay.style.visibility = "visible"
     popup.style.display = "flex"
-    FetchPopup()
+
+    const works = await FetchWorks()
+        let html = "";
+            works.forEach(elt => {
+            html += '<figure data-id="' + elt.id + '"><span><i class="fa-solid fa-trash-can delete-logo" data-id="'+ elt.id +'" ></i><img src="'+elt.imageUrl+'" class="popup-images"></span></figure>';
+        });
+            document.getElementById('popup-gallery').innerHTML = html;
+            const deleteBtns = document.querySelectorAll('.fa-trash-can');           
+            deleteBtns.forEach(trash => {
+                trash.addEventListener('click', (e) => {
+                    var idToDelete = e.target.getAttribute('data-id')
+                // deleteWork(e.target.getAttribute('data-id'))
+               const toDelete = document.querySelectorAll('figure[data-id="'+idToDelete+'"]')
+               toDelete.forEach(e => {
+                e.remove()
+               })
+                })
+            })         
 }
 
-// Fonction pour fermer la popup
+document.getElementById('edit-banner').addEventListener("click", openPopup)
+document.getElementById('div-edit').addEventListener("click", openPopup)
+
+// Fermer popup
 function closePopup() {
     const overlay = document.getElementById('overlay');
     const popup = document.getElementById('popup')
 
     overlay.style.visibility = "hidden"
     popup.style.display = "none"
+}
+
+
+
+async function deleteWork(id) {   
+    try {
+        const response = await fetch(`http://localhost:5678/api/works/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("token")
+            },
+            body:null
+        });
+        console.log(response)
+        if (response.ok) {
+            console.log("Work deleted successfully");
+        } else {
+            console.error("Failed to delete work");
+        }
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
 }
